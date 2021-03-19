7d72f93fcc8683d2f29ba190c7c9e27b89937017
Ahmed Shaban <shabanahmed491@gmail.com>
2021-03-19 14:38:41 +0200

    Add project files.

---
+++ Core/Entity/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Core.Entity
+{
+    public class EntityBase {
+
+        public Guid Id { get; set; }
+    }
+
+}
---
+++ Core/Entity/PortFolioItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entity
+{
+    public class PortFolioItem :EntityBase
+    {
+
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
